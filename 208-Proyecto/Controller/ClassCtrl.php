1834d2298fe1d9b6dcc6bccbc3dc1fd252f321f4
Blurred-9L <earthjenei@gmail.com>
2013-12-03 23:00:00 -0600

    Started documentation.

--- 208-Proyecto/Controller/ClassCtrl.php
+++ 208-Proyecto/Controller/ClassCtrl.php
@@ -1,16 +1,52 @@
 <?php
 
+/** @file ClassCtrl.php
+    @author Rodrigo Fuentes Hernandez <earthjenei@gmail.com>
+
+    This file contains the Controller class for
+    interaction with 'class' (as in school courses)
+    operations.
+*/
+
 require_once( 'Controller/DefaultCtrl.php' );
 
-class ClassCtrl extends DefaultCtrl{
-    private $model;
+/** @brief The ClassCtrl class is the Controller class
+    for class operations.
     
+    The ClassCtrl class is a direct child class of the
+    DefaultCtrl class. The ClassCtrl class is in charge
+    of functioning as the controller of class operations
+    in an MVC design patter. Supported operations are:
+    creating a new class, showing all of a teacher's
+    classes, showing specific info about a teacher's
+    class, seeing the class' roll and taking the class'
+    roll.
+*/
+class ClassCtrl extends DefaultCtrl{
+    private $model;             ///< Class model
+    
+    /** @brief ClassCtrl constructor.
+    
+        ClassCtrl constructor. Calls its parent constructor
+        to initialize the session as well as initializing
+        its own model.
+    */
     public function __construct(){
         parent::__construct();
         require_once( 'Model/ClassMdl.php' );
         $this -> model = new ClassMdl();
     }
     
+    /** @brief Selects the action to do depending on the
+        received GET action.
+        
+        The execute() method is in charge of selecting the
+        correct action according the value given during
+        a GET request to this controller. Permissions are
+        verified before executing the action, redirecting
+        the user to the login view if the user does not have
+        the required permissions to perform that action.
+    */
     public function execute(){
         switch( $_GET['action'] ){
             case 'new':
@@ -64,14 +100,23 @@ class ClassCtrl extends DefaultCtrl{
         }
     }
     
-    private function newClass(){
+    /** @brief Creates a new class for the current teacher.
+    
+        The newClass() method is called in order to insert a new
+        class for the current teacher. Several things are processed
+        during an insertion of a teacher's class, including the
+        class' schedules and evaluation parameters. 
+    */
+    protected function newClass(){
         if( empty( $_POST ) ){
+            /// Loads view if no data has been received.
             require_once( 'View/Profesores/nuevoCurso.html' );
         }
         else{
             $key = $_POST['new-class-key'];
             $prof = $_SESSION['user_id'];
             $cycle = $this -> model -> getLatestCycle();
+            /// Gets latest section value.
             $row = $this -> model -> getClassSectionValue( $key, $prof, $cycle );
             if( isset( $row['seccion'] ) ){
                 $section = $row['seccion'] + 1;
@@ -82,6 +127,7 @@ class ClassCtrl extends DefaultCtrl{
             
             $result = $this -> setUpNewClass( $key, $prof, $cycle, $section );
             if( $result === TRUE ){
+                /// Loads view.
                 $this -> processShowClassView( $key, $prof, $cycle, $section );
             }
             else{
@@ -90,10 +136,23 @@ class ClassCtrl extends DefaultCtrl{
         }
     }
     
+    /** @brief Processes view to show all classes related to a teacher.
+    
+        The processShowClassView() method replaces values on a template
+        in order to show the main info from an specific class belonging
+        to the current active teacher.
+        @param key : The class respective key.
+        @param prof : The id of the current active teacher.
+        @param cycle : The current active cycle string.
+        @param section : The inserted class' section.
+    */
     public function processShowClassView( $key, $prof, $cycle, $section ){
         $view = file_get_contents( 'View/Profesores/verCurso.html' );
+        /// Gets cycle information.
         $cycleRow = $this -> model -> getCycle( $cycle );
+        /// Gets related class information.
         $classRow = $this -> model -> getClass( $key );
+        /// Gets this class information.
         $teacherClassRow = $this -> model -> getTeacherClass( $prof, $cycle, $key, $section );
         
         $dict = array( '*class*' => $classRow['nombre'], '*cycle*' => $cycleRow['ciclo'], '*section*' => $section );
@@ -136,6 +195,21 @@ class ClassCtrl extends DefaultCtrl{
         echo $view;
     }
     
+    /** @brief Handles the insertion of the new class on the database.
+    
+        The setUpNewClass() method will be in charge of making the insertion
+        of the new class on the database, and, if the insertion was successful,
+        continue with the next required operations: inserting the class'
+        schedules and evaluation parameters.
+        @param key : The class respective key.
+        @param prof : The id of the current active teacher.
+        @param cycle : The current active cycle string.
+        @param section : The inserted class' section.
+        @return Returns TRUE if this and all succeeding operation are
+        successful, otherwise, returns FALSE.
+        @sa setUpSchedules()
+        @sa setUpEvaluations()
+    */
     private function setUpNewClass( $key, $teacher, $cycle, $section ){
         $result = $this -> model -> registerNewClass( $key, $teacher, $cycle, $section );
         
@@ -148,6 +222,18 @@ class ClassCtrl extends DefaultCtrl{
         }
     }
     
+    /** @brief Handles the insertion of the new class' schedules on
+        the database.
+        
+        The setUpSchedules() method takes care of receiving all the
+        information given about the new class' schedules on the POST
+        request, and tries to insert new schedule times on the database.
+        If all insertions are successful, this method will call the
+        setUpEvaluations() method
+        @return Returns TRUE if this and all succeeding operation are
+        successful, otherwise, returns FALSE.
+        @sa setUpEvaluations()
+    */
     private function setUpSchedules(){
         $count = 0;
         $indexKey = 'new-class-day' . $count;
@@ -155,11 +241,14 @@ class ClassCtrl extends DefaultCtrl{
         $teacherClassId = $this -> model -> insertId();
         
         while( array_key_exists( $indexKey, $_POST ) ){
+            /// Gets day, start time and class duration from POST.
             $day = $_POST['new-class-day' . $count];
             $start = $_POST['new-class-start' . $count];
             $duration = $_POST['new-class-duration' . $count];
+            /// Checks for schedule existance.
             $scheduleRow = $this -> model -> scheduleExists( $day, $start, $duration );
             if( $scheduleRow === FALSE ){
+                /// Inserts schedule if it does not exist.
                 $result = $this -> model -> registerSchedule( $day, $start, $duration );
                 if( $result === TRUE ){
                     $schedule = $this -> model -> insertId();
@@ -190,18 +279,34 @@ class ClassCtrl extends DefaultCtrl{
         }
     }
     
+    /** @brief Handles the insertion of class evaluation parameters
+        to the database.
+        
+        The setUpEvaluations() method will take care of the last
+        operation needed to create a new class for a teacher: the
+        creationg of evaluation pages and sub-evaluation elements.
+        Each class can have one or more evaluation pages, each one
+        having up to n elements. The insertion of this information
+        to the database is done on this method.
+        @param teacherClassId : The ID of the newly inserted class.
+        @return Returns TRUE if all insertions were sucessful,
+        otherwise returns FALSE.
+    */
     private function setUpEvaluations( $teacherClassId ){
         $count = 0;
         $indexKey = 'new-class-act' . $count;
         $fail = FALSE;
         
         while( array_key_exists( $indexKey, $_POST ) ){
+            /// Gets evaluation page information from POST.
             $act = $_POST['new-class-act' . $count];
             $val = $_POST['new-class-val' . $count];
             if( array_key_exists( 'page-columns' . $count, $_POST ) ){
+                /// Gets number of elements if given.
                 $nElems = $_POST['page-columns' . $count];
             }
             else{
+                /// Default number of elements.
                 $nElems = '1';
             }
             $result = $this -> model -> registerEvaluation( $act, $val, $nElems, $teacherClassId );
@@ -222,7 +327,15 @@ class ClassCtrl extends DefaultCtrl{
         }
     }
     
-    private function showTeacherAll(){
+    /** @brief Creates view from template to show all classes related
+        to a teacher.
+        
+        The showTeacherAll method will create a table from a template
+        in order to show the active teacher's classes and some of their
+        information including their class key, name, section, school
+        cycle to which they belong and academy.
+    */
+    protected function showTeacherAll(){
         $view = file_get_contents( 'View/Profesores/verCursos.html' );
         
         $teacherId = $_SESSION['user_id'];
@@ -249,7 +362,16 @@ class ClassCtrl extends DefaultCtrl{
         echo $view;
     }
     
-    private function showClass(){
+    /** @brief Collects data about a teacher's class in order to
+        show it.
+        
+        The showClass() method receives from a POST requests the
+        information about a class that the user wants to see.
+        In order to do this, several queries to the database are
+        made and then a call to process the data is done.
+        @sa processShowClassView()
+    */
+    protected function showClass(){
         $classKey = $_POST['classCode'];
         $section = $_POST['classSec'];
         $cycle = $_POST['classCycle'];
@@ -264,17 +386,32 @@ class ClassCtrl extends DefaultCtrl{
         $this -> processShowClassView( $classId, $teacherId, $cycleId, $section );
     }
     
-    private function getClassRollView(){
+    /** @brief Gets the view required to see a teacher's class
+        roll.
+        
+        Gets the view required to see a teacher's class roll.
+    */
+    protected function getClassRollView(){
         $view = file_get_contents( 'View/Profesores/verAsistencias.html' );
         
         echo $view;
     }
     
-    private function takeClassRoll(){
+    /** @brief Updates students signed up to a teacher's class
+        assistance.
+        
+        The takeClassRoll() method receives by POST request a set of
+        student codes, specifying the students that will have an
+        assistance during a class on a specific date. An update is
+        done to the database, modifying rows in the table related to
+        a students assistances.
+    */
+    protected function takeClassRoll(){
         if( empty( $_POST ) ){
             require_once( 'View/Profesores/tomarAsistencias.html' );
         }
         else{
+            /// Gets class info.
             $classInfo = $_POST['class-select'];
             $classArray = explode( '-', $classInfo );
             $classKey = $classArray[0];
@@ -297,11 +434,14 @@ class ClassCtrl extends DefaultCtrl{
             $okUpdates = 0;
             $key = "code-$count";
             while( array_key_exists( $key, $_POST ) ){
+                /// Gets student info by using the code as a key.
                 $code = $_POST[$key];
                 $studentRow = $this -> model -> getStudentByCode( $code );
                 $studentId = $studentRow['idAlumno'];
+                /// Gets signed up student's info.
                 $studentClassRow = $this -> model -> getStudentInClass( $studentId, $teacherClassId );
                 $studentClassId = $studentClassRow['idAlumnoCurso'];
+                /// Registers the assistance.
                 $result = $this -> model -> registerAssistance( $studentClassId, $date );
                 if( $result === TRUE ){
                     if( $this -> updateTotalAssistance( $studentClassId ) === TRUE ){
@@ -321,7 +461,16 @@ class ClassCtrl extends DefaultCtrl{
         }
     }
     
-    private function updateTotalAssistance( $studentClassId ){
+    /** @brief Updates a student's class info to show an update over
+        his or her class assistance.
+        
+        The updateTotalAssistance() method updates the field on the
+        student's class table to show the current percentage of assistance
+        acquired up to the latest update over the class assistance.
+        @param studentClassId : the ID of the student signed up class.
+        @return Returns TRUE if successful, otherwise returns FALSE.
+    */
+    public function updateTotalAssistance( $studentClassId ){
         $assistances = $this -> model -> getStudentAssistances( $studentClassId );
         $nAssistances = count( $assistances );
         $total = 0;
@@ -336,7 +485,16 @@ class ClassCtrl extends DefaultCtrl{
         return $result;
     }
     
-    private function cloneClass(){
+    /** @brief Clones a class parameters into a new class.
+    
+        The cloneClass() method is used whenever a teacher is to lazy
+        to actually set up evaluation parameters and schedules of a new
+        class again. The operation is given as something convenient,
+        allowing the teacher to clone existing parameters into a new
+        class. All operations done when creating a new class are also
+        done when cloning a class.
+    */
+    protected function cloneClass(){
         if( empty( $_POST ) ){
             require_once( 'View/Profesores/clonarCurso.html' );
         }
--- 208-Proyecto/Controller/DefaultCtrl.php
+++ 208-Proyecto/Controller/DefaultCtrl.php
@@ -1,12 +1,43 @@
 <?php
 
+/** @file DefaultCtrl.php
+    @author Rodrigo Fuentes Hernandez <earthjenei@gmail.com>
+
+    This file contains the default Controller class
+    from which all other Controllers used inherit from.
+*/
+
+/** @brief The DefaultCtrl class defines basic behaviour for
+    all controllers used.
+    
+    The DefaultCtrl class defines basic behaviour for all
+    controllers used inside the application. This basic behaviour
+    includes session handling, checking for permissions,
+    checking for the existance of a session and redirecting a
+    user without the required permissions.
+*/
 class DefaultCtrl{
+
+    /** @brief DefaultCtrl constructor.
+    
+        The DefaultCtrl constructor method. Initializes the
+        session by calling session_start.
+    */
     public function __construct(){
         session_start();
     }
     
+    /** @brief Check the existance of a session.
+    
+        The checkSession() method will check for the existance
+        of three values stored in the PHP $_SESSION array.
+        These values are the user's id, the user's code and
+        the type of user that holds the current session.
+        @return Returns TRUE if a session exists, otherwise,
+        returns FALSE.
+    */
     public function checkSession(){
-        // A session will store... id and type.
+        // A session will store id, type of user and user code.
         $sessionExists = FALSE;
         
         if( isset( $_SESSION['user_code'] ) && isset( $_SESSION['user_type'] ) && isset( $_SESSION['user_id'] ) ){
@@ -16,27 +47,50 @@ class DefaultCtrl{
         return $sessionExists;
     }
     
+    /** @brief Redirects user according to permission level.
+    
+        The redirectUser() method is in charge of loading the main
+        page of each of the different permission levels.
+    */
     public function redirectUser(){
         switch( $_SESSION['user_type'] ){
-            case 'rookie': // Student
+            case 'rookie': /// Student
                 require_once( 'View/Alumnos/alumnosMain.html' );
                 break;
-            case 'brigadier': // Teacher
+            case 'brigadier': /// Teacher
                 require_once( 'View/Profesores/profesoresMain.html' );
                 break;
-            case 'ninja': // Admin
+            case 'ninja': /// Admin
                 require_once( 'View/Admin/adminMain.html' );
                 break;
         }
     }
     
+    /** @brief Kills the current session.
+    
+        Kills the current session by unsetting all its variables,
+        destroying the session and ensuring that the cookie related
+        to the session is eliminated.
+    */
     public function killSession(){
-        session_unset();        // Unsets session
-        session_destroy();      // Destroys session
-        setcookie( session_name(), '', time() - 3600 );     // Kills cookie
-        header( 'Location: ./index.php?ctrl=login&action=login' );
+        session_unset();                                                /// Unsets session
+        session_destroy();                                              /// Destroys session
+        setcookie( session_name(), '', time() - 3600 );                 /// Kills cookie
+        header( 'Location: ./index.php?ctrl=login&action=login' );      /// Changes header location.
     }
     
+    /** @brief Checks if a user's permissions level match up with
+        the expected permissions level.
+        
+        Checks the user's permission level against a given string
+        representing the expected permission level that the user
+        should have in order to access a certain functionality or
+        operation.
+        @param userType : a string indicating the expected permission
+        level. It can be: 'rookie', 'brigadier' or 'ninja'.
+        @return Returns TRUE if the permission level matches up,
+        otherwise, returns FALSE.
+    */
     public function checkPermissions( $userType ){
         $ok = FALSE;
         
@@ -49,6 +103,15 @@ class DefaultCtrl{
         return $ok;
     }
     
+    /** @brief Selects the action to do depending on the
+        received GET action.
+        
+        The execute() method is in charge of selecting the
+        correct action according the value given during
+        a GET request to this controller. Since this is the
+        default controller, the only possible action is
+        exiting the system.
+    */
     public function execute(){
         switch( $_GET['action'] ){
             case 'exit':
